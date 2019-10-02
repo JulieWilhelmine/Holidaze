d8deea98853516e433e2c3567242e3fdcaa4dc9b
Julie Wilhelmine <juliewilhelmine@gmail.com>
2019-10-02 14:32:33 +0200

    AddEstablishment updates

--- src/pages/AddEstablishmentPage.jsx
+++ src/pages/AddEstablishmentPage.jsx
@@ -12,30 +12,38 @@ class AddEstablishmentPage extends Component {
             <h1 className="[ headline ] [ headline--admin ]">Administration</h1>
                 
                 <h2 className="[ headline ] [ headline--addEstablishment ]">Establishment details:</h2>
-                    <form method="POST" action="http://localhost/hotel-booking/server/add-establishments-success.php">
+                    <form method="POST" action="http://localhost/hotel-booking/server/add-establishments-success.php" className="[ addEstablishment ]">
                         <label for="establishmentName">Establishment Name: </label>
-                        <input type="text" name="establishmentName" id="establishmentName" className="[ addEstablishment__input ]" />
+                        <input type="text" name="establishmentName" id="establishmentName" className="[ addEstablishment__input ]" required />
                         <label for="establishmentEmail">Establishment Email: </label>
-                        <input type="text" name="establishmentEmail" id="establishmentEmail" className="[ addEstablishment__input ]" />
+                        <input type="text" name="establishmentEmail" id="establishmentEmail" className="[ addEstablishment__input ]" required />
+                        <br />
                         <label for="imageUrl">Image URL: </label>
-                        <input type="text" name="imageUrl" id="imageUrl" className="[ addEstablishment__input ]" />
+                        <input type="text" name="imageUrl" id="imageUrl" className="[ addEstablishment__input ]" required />
                         <label for="price">Price per person per night ($): </label>
-                        <input type="number" name="price" id="price" className="[ addEstablishment__input ]" />
+                        <input type="number" name="price" id="price" className="[ addEstablishment__input ]" required />
+                        
+                        <br />
                         <label for="maxGuests">Max guests: </label>
-                        <input type="number" name="maxGuests" id="maxGuests" className="[ addEstablishment__input ]" />
-                        <label for="googleLat">Google Coordinates Latitude: </label>
-                        <input type="text" name="googleLat" id="googleLat" className="[ addEstablishment__input ]" />
-                        <label for="googleLong">Google Coordinates Longitude: </label>
-                        <input type="text" name="googleLong" id="googleLong" className="[ addEstablishment__input ]" />
-                        <label for="description">Description: </label>
-                        <textarea name="description" id="description" rows="8" cols="80" className="[ addEstablishment__input ]" />
-                        <label for="selfCatering">Self-catering?</label>
-                        <label for="true">True</label>
-                        <input type="radio" id="true" name="selfCatering" value="true" />
-                        <label for="false">False</label>
-                        <input type="radio" id="false" name="selfCatering" value="false" />
+                        <input type="number" name="maxGuests" id="maxGuests" className="[ addEstablishment__input ]" required />
                         <label for="id">ID: </label>
-                        <input type="number" name="id" id="id" className="[ addEstablishment__input ]" />
+                        <input type="number" name="id" id="id" className="[ addEstablishment__input ]" required />
+                        <br />
+                        <label for="selfCatering">Self-catering?</label>
+                        <label for="true">Yes</label>
+                        <input type="radio" id="true" name="selfCatering" value="true" />
+                        <label for="false">No</label>
+                        <input type="radio" id="false" name="selfCatering" value="false" />
+                        <br />
+                        <label for="googleLat">Google Coordinates Latitude: </label>
+                        <input type="text" name="googleLat" id="googleLat" className="[ addEstablishment__input ]" required />
+                        <br />
+                        <label for="googleLong">Google Coordinates Longitude: </label>
+                        <input type="text" name="googleLong" id="googleLong" className="[ addEstablishment__input ]" required />
+                        <br />
+                        <label for="description">Description: </label>
+                        <input name="description" id="description" className="[ addEstablishment__input ]" required />
+                        <br />
                         <input type="submit" className="[ addEstablishment__submit ]"/>
                     </form>
         </div>
