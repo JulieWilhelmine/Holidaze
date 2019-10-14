048065bba43e7957603203fcb12052fedb8308f0
Julie Wilhelmine <juliewilhelmine@gmail.com>
2019-10-14 12:30:47 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,6 +8,32 @@ import 'react-bootstrap-typeahead/css/Typeahead.css';
 
 class HomePage extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+        hotelArray: []
+    };
+}
+
+  componentDidMount() {
+    this.getData();
+}
+
+getData = () => {
+
+    fetch('http://localhost/hotel-booking/server/get-establishments.php')
+        .then(response => {
+            return response.json()
+        })
+        .then(result => {
+            console.log('result', result);
+
+            this.setState({
+                hotelArray: result
+            });
+        });
+}
+
   render() {
     return (
         <div className="[ row ]">
@@ -17,7 +43,7 @@ class HomePage extends Component {
             onChange={(selected) => {
             // Handle selections...
             }}
-            options={[ /* Array of objects or strings */ ]}
+            options={this.state.hotelArray}
             placeholder="What are you looking for?"
         />
         </div>
