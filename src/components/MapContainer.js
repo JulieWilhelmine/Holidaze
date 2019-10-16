0901c63410c420eeadf137648ca2a355be18c8ec
Julie Wilhelmine <juliewilhelmine@gmail.com>
2019-10-16 12:00:31 +0200

    google maps

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,22 +1,24 @@
 // Google maps component
 
 import React from "react";
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 const mapStyles = {
   width: '20%',
   height: '15%',
 };
 
-function MapContainer() {
+function MapContainer(props) {
    
         return (
-            <Map
-              google={this.props.google}
-              zoom={8}
-              style={mapStyles}
-              initialCenter={{ lat: {this.props.lat}, lng: {this.props.long}}}
-            />
+          <Map
+          google={props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: {props.lat}, lng: {props.long}}}
+        >
+          <Marker position={{ lat: {props.lat}, lng: {props.long}}} />
+        </Map>
         );
       
 }
