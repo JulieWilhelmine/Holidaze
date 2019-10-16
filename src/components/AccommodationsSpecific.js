b424e554eb78cdaae6948896d1529adedf0316c4
cnnrbrn <connorbobrien@gmail.com>
2019-10-16 15:28:20 +0200

    Map layout changes

--- src/components/AccommodationsSpecific.js
+++ src/components/AccommodationsSpecific.js
@@ -4,39 +4,30 @@ import React from "react";
 import MapContainer from "../components/MapContainer";
 
 export default class AccommodationSpecific extends React.Component {
-  render() {
-    return (
-    <div className="[ container-fluid ]">
-      <div className="[ row ]">
-        <div className="[ col-sm-12 ]">
-          <br />
-          <br />
-          <br />
-        </div>
-        <div className="[ col-sm-6 ]">
-          <img
-            src={this.props.image}
-            className="[ hotel__image ] [ img-fluid ]"
-            alt={this.props.name}
-          />
-        </div>
-        <div className="[ col-sm-6 ] [ hotel__info ]">
-          <h2 className="[ hotel__name ]">{this.props.name}</h2>
-          <h2 className="[ hotel__price ]">{this.props.price + "$"}</h2>
-          <br />
-          <p className="[ hotel__description ]">{this.props.description}</p>
-          <b className="[ hotel__email ]">Email:</b> <p>{this.props.email}</p>
-          <b className="[ hotel__location ]">Location:</b>
-          <MapContainer
-                    lat={this.props.lat}
-                    long={this.props.long}
-          />
-          <button className="[ enquiryButton ]" onClick={this.props.openModal}>
-            Make an enquiry!
-          </button>
-        </div>
-      </div>
-    </div>
-    );
-  }
+	render() {
+		return (
+			<div className="[ container ] [ accommodation ]">
+				<div className="[ row ]">
+					<div className="[ col-md-6 ]">
+						<img src={this.props.image} className="[ hotel__image ] [ img-fluid ]" alt={this.props.name} />
+					</div>
+					<div className="[ col-md-6 ] [ hotel__info ]">
+						<div class="[ hotel__info ]">
+							<h2 className="[ hotel__name ]">{this.props.name}</h2>
+							<h2 className="[ hotel__price ]">{this.props.price + "$"}</h2>
+							<p className="[ hotel__description ]">{this.props.description}</p>
+							<b className="[ hotel__email ]">Email:</b> <p>{this.props.email}</p>
+							<b className="[ hotel__location ]">Location:</b>
+							<div className="[ map ]">
+								<MapContainer lat={this.props.lat} long={this.props.long} />
+							</div>
+							<button className="[ enquiryButton2 ]" onClick={this.props.openModal}>
+								Make an enquiry!
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,23 +1,16 @@
 // Google maps component
 
 import React from "react";
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
 function MapContainer(props) {
-   
-        return (
-          <Map className="googleMap"
-          google={props.google}
-          zoom={11}
-          initialCenter={{ lat: props.lat, lng: props.long}}
-        >
-          <Marker position={{ lat: props.lat, lng: props.long}} />
-        </Map>
-        );
-      
+	return (
+		<Map className="map__google" google={props.google} zoom={11} initialCenter={{ lat: props.lat, lng: props.long }}>
+			<Marker position={{ lat: props.lat, lng: props.long }} />
+		</Map>
+	);
 }
 
 export default GoogleApiWrapper({
-    apiKey: 'AIzaSyDcbKy88ktKrQ9jYW-FiRrwlRNtYMaHJLk'
-  })(MapContainer);
+	apiKey: "AIzaSyDcbKy88ktKrQ9jYW-FiRrwlRNtYMaHJLk"
+})(MapContainer);
