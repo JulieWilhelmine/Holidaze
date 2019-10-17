61494f2186faccc7aa10f9a27aafa38ab7158ea7
Julie Wilhelmine <juliewilhelmine@gmail.com>
2019-10-17 10:48:48 +0200

    css updates

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -5,7 +5,7 @@ import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
 function MapContainer(props) {
 	return (
-		<Map className="map__google" google={props.google} zoom={11} initialCenter={{ lat: props.lat, lng: props.long }}>
+		<Map className="[ map__google ]" google={props.google} zoom={11} initialCenter={{ lat: props.lat, lng: props.long }}>
 			<Marker position={{ lat: props.lat, lng: props.long }} />
 		</Map>
 	);
